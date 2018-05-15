4bb0b741730c456a1c2bb2476f72139700aeb2a6
Dimwest <dimwestdev@gmail.com>
2018-05-15 18:10:08 +0200

    Added if case checkup

--- data_engineering/tests/migrate_script.sh
+++ data_engineering/tests/migrate_script.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 if [ $TRAVIS_BRANCH = "test" ]; then 
+  echo 'On branch test'
   cd flyway-5.0.7 && ./flyway -url=$STAGING_URL -user=$STAGING_USER -password=$STAGING_PW -locations=filesystem:../analyst/dwh/incremental_migrations -schemas=$SCHEMAS migrate
-elif [ $TRAVIS_BRANCH = "master" ]; then 
+elif [ $TRAVIS_BRANCH = "master" ]; then
+  echo 'On branch master' 
   cd flyway-5.0.7 && ./flyway -url=$PROD_URL -user=$PROD_USER -password=$PROD_PW -locations=filesystem:../analyst/dwh/incremental_migrations -schemas=$SCHEMAS migrate
 fi
