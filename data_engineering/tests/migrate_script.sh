11d799c8815d88ab03e9acdd709e3c833c108070
Dimwest <dimwestdev@gmail.com>
2018-05-15 17:51:37 +0200

    Testing bash script exec

---
+++ data_engineering/tests/migrate_script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+if [ $TRAVIS_BRANCH = "test" ] then 
+  cd flyway-5.0.7 && ./flyway -url=$STAGING_URL -user=$STAGING_USER -password=$STAGING_PW -locations=filesystem:../analyst/dwh/incremental_migrations -schemas=$SCHEMAS migrate 
+elif [ $TRAVIS_BRANCH = "master" ] then 
+  cd flyway-5.0.7 && ./flyway -url=$PROD_URL -user=$PROD_USER -password=$PROD_PW -locations=filesystem:../analyst/dwh/incremental_migrations -schemas=$SCHEMAS migrate 
+fi
